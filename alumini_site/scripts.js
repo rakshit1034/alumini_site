88c19f2820eeec41717dcd7339fe67080bce6bfa
rakshit1034 <154755492+rakshit1034@users.noreply.github.com>
2023-12-24 23:25:10 +0530

    Add files via upload

---
+++ alumini_site/scripts.js
@@ -0,0 +1,195 @@
+const candidates = [
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Raman Rajput",
+    profilePicture:
+      "D:ProjectsHotel-management_sitealumini_siteassetsmyprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  {
+    name: "Rakshit Rajput",
+    profilePicture: "./assets/myprofilepic.jpg",
+    resume: "path/to/resume1.pdf",
+    linkedin: "https://www.linkedin.com/in/rakshit-rajput-02ab51198/",
+    github: "https://github.com/Rakshit-rajput",
+    about: "Aspiring web developer and DSA enthusiast",
+    section: "B",
+    project: "https://crypto-app-gilt-rho.vercel.app/",
+  },
+  // Add more candidates as needed
+];
+
+const candidatesContainer = document.getElementById("candidatesContainer");
+const candidateModal = document.getElementById("candidateModal");
+const modalContent = document.getElementById("modalContent");
+
+// Function to create candidate cards
+function createCandidateCard(candidate) {
+  const card = document.createElement("div");
+  card.classList.add("candidate-card");
+  card.innerHTML = `
+        <img src="${candidate.profilePicture}" alt="${candidate.name}">
+        <h3>${candidate.name}</h3>
+        <h6>${candidate.about}</h6>
+        
+    `;
+  card.addEventListener("click", () => openModal(candidate));
+  return card;
+}
+
+// Function to display detailed candidate information in the modal
+function openModal(candidate) {
+  modalContent.innerHTML = `
+        <span class="close" onclick="closeModal()">&times;</span>
+        <img src="${candidate.profilePicture}" alt="${candidate.name}">
+        <h2>${candidate.name}</h2>
+        <p>Section: ${candidate.section}</p>
+        <p>Resume: <a href="${candidate.resume}" target="_blank">View Resume</a></p>
+        <p>LinkedIn: <a href="${candidate.linkedin}" target="_blank">${candidate.linkedin}</a></p>
+        <p>GitHub: <a href="${candidate.github}" target="_blank">${candidate.github}</a></p>
+        <p>Project: <a href="${candidate.project}" target="_blank">${candidate.project}</a></p>
+    `;
+  candidateModal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+  candidateModal.style.display = "none";
+}
+
+// Initialize the webpage with candidate cards
+candidates.forEach((candidate) => {
+  candidatesContainer.appendChild(createCandidateCard(candidate));
+});
